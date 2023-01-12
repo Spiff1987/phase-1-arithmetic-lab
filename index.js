1631a7de078dbf929c2e784d7cac338cc06c113b
dismas <afubwa@gmail.com>
2023-01-12 17:19:01 +0400

    Jvascript arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 
 // Write your code here
+// Multiplication
+
+const num1 = 15.5;
+
+const num2 = 4;
+
+const multiply = (num1 * num2);
+
+
+// Interger
+
+let random = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+
+console.log(random);
+
+
+
+// Remainder
+
+const num4 = 12;
+
+const num3 = 8;
+
+const mod = (num4 % num3);
+
+
+// Max
+
+let set = [10, 5, 20, 2, 8];
+
+let max = Math.max(...set);
+
+console.log(max); // Output: 20
